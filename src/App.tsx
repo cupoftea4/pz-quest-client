5bbc5df31224c5a2fcdbf92ddab71e34eb620163
Oleksii Morozov <95098796+Oleksii-Morozov@users.noreply.github.com>
2023-09-10 19:50:21 +0300

    [feat] new design

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,33 @@ import './App.css'
 
 function App() {
   const [answer, setAnswer] = useState("")
+  const [task] = useState("Current task")
   const [taskNumber, setTaskNumber] = useState(0)
+  const [answerComplexity, setAnswerComplexity] = useState("easy");
 
   function submitAnswer() {
     console.log(answer)
+    console.log(answerComplexity)
     setTaskNumber(taskNumber + 1)
   }
 
   return (
     <>
       <div className="App">
-        <h1>Завдання {taskNumber + 1}</h1>
-        <input placeholder="Введіть відповідь сюди" type="text" className='input' onChange={e => setAnswer(e.currentTarget.value)}/>
-        <button onClick={() => submitAnswer()}>
-          Перевірити
-        </button>
+        <div className='container'>
+          <input type='checkbox' className='switch' onChange={() => setAnswerComplexity(answerComplexity === "easy" ? "hard" : "easy")}/>
+          <h1>Завдання {taskNumber + 1}</h1>
+          <div>
+            <img className='image' src=""/>
+          </div>
+          <div>{task}</div>
+          <input placeholder="Введіть відповідь сюди" type="text" className='input' onChange={e => setAnswer(e.currentTarget.value)}/>
+        </div>
+        <div>
+          <button onClick={() => submitAnswer()}>
+            Перевірити
+          </button>
+        </div>
       </div>
     </>
   )
