f712e0f65cceb6537d78357aefe4cba4eec11afd
Oleksii Morozov <95098796+Oleksii-Morozov@users.noreply.github.com>
2023-09-10 19:51:01 +0300

    [feat] new design (not bad commit)

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,41 @@ function App() {
   const [answer, setAnswer] = useState("")
   const [task] = useState("Current task")
   const [taskNumber, setTaskNumber] = useState(0)
-  const [answerComplexity, setAnswerComplexity] = useState("easy");
+  const [isHard, setIsHard] = useState(false)
 
   function submitAnswer() {
     console.log(answer)
-    console.log(answerComplexity)
     setTaskNumber(taskNumber + 1)
   }
 
   return (
     <>
-      <div className="App">
+      <div className="app">
         <div className='container'>
-          <input type='checkbox' className='switch' onChange={() => setAnswerComplexity(answerComplexity === "easy" ? "hard" : "easy")}/>
-          <h1>Завдання {taskNumber + 1}</h1>
-          <div>
-            <img className='image' src=""/>
+          <div className="switch-button" onClick={() => setIsHard(!isHard)}>
+            <span className={`active ${isHard && "switch-active"}`}></span>
+            <button className={`switch-button-case left ${!isHard && "active-case"}`}>Easy</button>
+            <button className={`switch-button-case right ${isHard && "active-case"}`}>Hard</button>
           </div>
-          <div>{task}</div>
-          <input placeholder="Введіть відповідь сюди" type="text" className='input' onChange={e => setAnswer(e.currentTarget.value)}/>
+          <div className='content'>
+            <h1>Завдання {taskNumber + 1}</h1>
+            <div>
+              <img className='image' src={""} alt="Task image"/>
+            </div>
+            <div>{task}</div>
+            <textarea
+              placeholder="Введіть відповідь сюди" rows={5} className='input' 
+              onChange={e => setAnswer(e.currentTarget.value)}/>
+          </div>
         </div>
         <div>
-          <button onClick={() => submitAnswer()}>
+          <button className="button submit" onClick={() => submitAnswer()}>
             Перевірити
           </button>
+          <button className="button" onClick={() => submitAnswer()}>
+            Пропустити
+          </button>
+          <div>Contact dev @aler1x @cupoftea4</div>
         </div>
       </div>
     </>
