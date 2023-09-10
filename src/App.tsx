56823dab3680ca3dbaecbf0e48a9677a1e6d42b0
cupoftea4 <nadiia.telko.pz.2021@lpnu.ua>
2023-09-10 15:22:08 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [answer, setAnswer] = useState("")
+  const [taskNumber, setTaskNumber] = useState(0)
+
+  function submitAnswer() {
+    console.log(answer)
+    setTaskNumber(taskNumber + 1)
+  }
+
+  return (
+    <>
+      <div className="App">
+        <h1>Завдання {taskNumber + 1}</h1>
+        <input type="text" className='input' onChange={e => setAnswer(e.currentTarget.value)}/>
+        <button onClick={() => submitAnswer()}>
+          Перевірити
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
